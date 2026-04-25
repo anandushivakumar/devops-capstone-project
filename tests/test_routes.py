5d06c17cd5bcf322f7f72fe8749927c91fd901e0
anandushivakumar <anandu.shivakumar@gmail.com>
2026-04-25 07:43:09 -0400

    fixed linting issues

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,7 +134,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-    
+
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
@@ -147,7 +147,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-    
+
     def test_update_account(self):
         """It should Update an existing Account"""
         test_account = AccountFactory()
@@ -162,7 +162,7 @@ class TestAccountService(TestCase):
 
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Updated Name")
-    
+
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
